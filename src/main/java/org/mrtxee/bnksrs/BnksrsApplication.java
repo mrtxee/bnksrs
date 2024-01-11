d32554492efbb7aaaac9d5d7c86c61adc0920d7e
mrtxee <mrtxee@bk.ru>
2024-01-11 17:42:36 +0300

    +

---
+++ src/main/java/org/mrtxee/bnksrs/BnksrsApplication.java
@@ -0,0 +1,13 @@
+package org.mrtxee.bnksrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnksrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnksrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mrtxee/bnksrs/BnksrsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mrtxee.bnksrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnksrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
